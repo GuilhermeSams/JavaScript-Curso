3bdfaaa80f26b11cc49fa0c7063f3107e73e8b89
GuilhermeSams <guilhermeps3670@gmail.com>
2023-04-21 14:45:38 -0300

    tipos de saída - alert, document.getElementById innerHTML, console.log e document.writer

--- aula01/js/index.js
+++ aula01/js/index.js
@@ -1,3 +1,3 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>"; // Quando queremos uma interação com o site geralemnte utilizamos o innerHTML
 console.log("Oi isso é um console.log");
-alert("Alertando")
+alert("Alertando") // outra forma de mostrar na tela.
