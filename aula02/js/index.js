8a5a629b4260163849ed21a67a6625ee5f9b4071
GuilhermeSams <guilhermeps3670@gmail.com>
2023-04-21 14:48:25 -0300

    var, var declaration lowercase and uppercase

---
+++ aula02/js/index.js
@@ -0,0 +1,19 @@
+/*
+var a=1;
+var b=5;
+var c=a+b;
+console.log(c);
+*/
+
+/*
+var primeiro-nome; <ERRADO
+var primeiro_nome; <CERTO
+var primeiroNome; <CERTO
+*/
+
+/*
+var PESSOAS = 'Dimitri'
+var pessoas = 'teixeira'
+
+console.log(pessoas)
+*/
