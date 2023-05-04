e0b7d2f386ecb19a80b066ddc0320c69b675ffd9
GuilhermeSams <guilhermeps3670@gmail.com>
2023-05-04 15:44:16 -0300

    function

---
+++ aula05/js/index.js
@@ -0,0 +1,45 @@
+/* //SOMA
+function soma(v1, v2) {
+    return v1 + v2
+}
+
+document.getElementById("texto").innerHTML = soma(10, 10)
+*/
+
+/* // COTAÇÃO DO DOLAR
+function realParaDolar (real, cotacaoDolar) {
+    return real * cotacaoDolar
+}
+
+document.getElementById("texto").innerHTML = realParaDolar(10, 5.05)
+*/
+
+/* COTAÇÃO DO DOLAR DIFERENTE
+function realParaDolar (real, cotacaoDolar) {
+    return real * cotacaoDolar
+}
+
+var valorReal = 7.89
+var cotacao = 5.08
+
+var total = realParaDolar(valorReal, cotacao)
+
+alert("O valor em real é R$:" +valorReal+" o valor em dólar é U$:"+total.toFixed(2))
+*/
+
+/* // Exportando uma função
+function alertaHello() {
+    alert("Olá pessoal");
+}
+
+*/
+
+/*
+function ParaCelsius(valorFahrenheit) {
+    return (5/9) * (valorFahrenheit - 32)
+}
+
+var x = ParaCelsius(77);
+
+alert("A temperatura é de " + x + " graus celsius")
+*/
